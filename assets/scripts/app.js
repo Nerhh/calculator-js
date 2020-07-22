9b7af63923f5d2cdd1dcd2c015d6ae2e350c6d12
Marcus Taylor-Deacon <marcus@deacondevs.com>
2020-07-23 00:19:35 +0100

    Refactoring

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,10 +6,12 @@ subtractBtn.addEventListener('click', subtractNum);
 multiplyBtn.addEventListener('click', multiplyNum);
 divideBtn.addEventListener('click', divideNum);
 
+// Extract input from field
 function userNumInput(){
     return parseInt(userInput.value);
 }
 
+// Writes calculation log
 function createAndWriteOutput(opp, resultBefore, calcNumber){
     const desc = `${resultBefore} ${opp} ${calcNumber}`
     outputResult(currentResult, desc);
@@ -18,27 +20,27 @@ function createAndWriteOutput(opp, resultBefore, calcNumber){
 function addNum(){
     const enteredNum = userNumInput();
     const initialResult = currentResult;
-    currentResult = currentResult + enteredNum;
+    currentResult += enteredNum;
     createAndWriteOutput('+', initialResult, enteredNum);
 }
 
 function subtractNum(){
     const enteredNum = userNumInput();
     const initialResult = currentResult;
-    currentResult = currentResult - enteredNum;
+    currentResult -= enteredNum;
     createAndWriteOutput('-', initialResult, enteredNum);
 }
 
 function multiplyNum(){
     const enteredNum = userNumInput();
     const initialResult = currentResult;
-    currentResult = currentResult * enteredNum;
+    currentResult *= enteredNum;
     createAndWriteOutput('*', initialResult, enteredNum);
 }
 
 function divideNum(){
     const enteredNum = userNumInput();
     const initialResult = currentResult;
-    currentResult = currentResult / enteredNum;
+    currentResult /= enteredNum;
     createAndWriteOutput('/', initialResult, enteredNum);
 }
