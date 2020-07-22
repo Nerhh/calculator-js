16dbf4a999a0236a7962dbec4df8dd70ffd7a7dd
Marcus Taylor-Deacon <marcus@deacondevs.com>
2020-07-23 00:11:35 +0100

    Subtract, Multiply, Divide Added

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,14 +2,43 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 
 addBtn.addEventListener('click', addNum);
+subtractBtn.addEventListener('click', subtractNum);
+multiplyBtn.addEventListener('click', multiplyNum);
+divideBtn.addEventListener('click', divideNum);
 
 function userNumInput(){
     return parseInt(userInput.value);
 }
 
+function createAndWriteOutput(opp, resultBefore, calcNumber){
+    const desc = `${resultBefore} ${opp} ${calcNumber}`
+    outputResult(currentResult, desc);
+}
+
 function addNum(){
     const enteredNum = userNumInput();
-    const desc = `${currentResult} + ${enteredNum}`
+    const initialResult = currentResult;
     currentResult = currentResult + enteredNum;
-    outputResult(currentResult, desc);
+    createAndWriteOutput('+', initialResult, enteredNum);
+}
+
+function subtractNum(){
+    const enteredNum = userNumInput();
+    const initialResult = currentResult;
+    currentResult = currentResult - enteredNum;
+    createAndWriteOutput('-', initialResult, enteredNum);
+}
+
+function multiplyNum(){
+    const enteredNum = userNumInput();
+    const initialResult = currentResult;
+    currentResult = currentResult * enteredNum;
+    createAndWriteOutput('*', initialResult, enteredNum);
+}
+
+function divideNum(){
+    const enteredNum = userNumInput();
+    const initialResult = currentResult;
+    currentResult = currentResult / enteredNum;
+    createAndWriteOutput('/', initialResult, enteredNum);
 }
